78f1a7b0e5c1607d08e88216781576bd7995080b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:04 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -25,33 +25,39 @@ import PackageDescription
 let package = Package(
     name: "swift-package-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Package Primitives",
             targets: ["Package Primitives"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-tagged-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-tagged-primitives.git",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
             name: "Package Primitives",
             dependencies: [
                 .product(name: "Tagged Primitives", package: "swift-tagged-primitives"),
-                .product(name: "Tagged Primitives Standard Library Integration", package: "swift-tagged-primitives"),
+                .product(
+                    name: "Tagged Primitives Standard Library Integration",
+                    package: "swift-tagged-primitives"
+                ),
             ]
         ),
         .testTarget(
             name: "Package Primitives Tests",
             dependencies: [
-                "Package Primitives",
+                "Package Primitives"
             ],
             path: "Tests/Package Primitives Tests"
         ),
